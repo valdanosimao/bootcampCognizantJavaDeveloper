8c3986d9ab021040dc29f06aed788bb3d312efb5
Valdano Simão <valdanosimao@gmail.com>
2021-12-07 11:22:02 -0300

    Estruturas de Dados em Java: Árvores

---
+++ src/com/bootcampCognizantJavaDeveloper/estruturaDeDados/arvores/Main.java
@@ -0,0 +1,23 @@
+package com.bootcampCognizantJavaDeveloper.estruturaDeDados.arvores;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		ArvoreBinaria<Obj> minhaArvore = new ArvoreBinaria<>();
+		
+		minhaArvore.inseir(new Obj(13));
+		minhaArvore.inseir(new Obj(10));
+		minhaArvore.inseir(new Obj(25));
+		minhaArvore.inseir(new Obj(2));
+		minhaArvore.inseir(new Obj(12));
+		minhaArvore.inseir(new Obj(20));
+		minhaArvore.inseir(new Obj(31));
+		minhaArvore.inseir(new Obj(29));
+		
+		minhaArvore.exibirInOrdem();
+		minhaArvore.exibirPreOrdem();
+		minhaArvore.exibirPosOrdem();
+	}
+
+}
---
+++ src/com/bootcampCognizantJavaDeveloper/estruturaDeDados/arvores/ObjArvore.java
@@ -0,0 +1,11 @@
+package com.bootcampCognizantJavaDeveloper.estruturaDeDados.arvores;
+
+public abstract class ObjArvore<T> implements Comparable<T> {
+	
+	public abstract boolean equals(Object o);
+	public abstract int hashCode();
+	public abstract int compareTo(T outro);
+	public abstract String toString();
+	
+
+}
